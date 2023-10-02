2f6f5c8be3b7c6e0fce54b202e5831ea59070577
brunolimabh <bruno.lima@sptech.school>
2023-10-02 19:40:33 -0300

    finalizando ex conta-corrente

--- conta-corrente/src/main/java/org/example/Main.java
+++ conta-corrente/src/main/java/org/example/Main.java
@@ -3,5 +3,12 @@ package org.example;
 public class Main {
     public static void main(String[] args) {
         System.out.println("Hello world!");
+        ContaCorrente conta = new ContaCorrente("bruno", 0.0);
+        conta.depositar(10.0);
+        conta.sacar(5.0);
+        conta.exibirExtrato();
+        System.out.println(conta);
     }
+
+
 }
