5461827aa93b2c644583f7cfe377ab948e93ffaf
brunolimabh <bruno.lima@sptech.school>
2023-10-05 12:29:14 -0300

    adicionando ex de teste em aula

---
+++ exercicio-cinema-com-testes/exercicio-cinema-com-testes/src/main/java/school/sptech/Cinema.java
@@ -0,0 +1,27 @@
+package school.sptech;
+
+import java.util.List;
+
+public class Cinema {
+    private String nome;
+
+    public Cinema(String nome) {
+        this.nome = nome;
+    }
+
+    public void adicionarSessao(Sessao sessao) {
+
+    }
+    public Integer getQuantidadeSessoes3d() {
+        return null;
+    }
+    public List<Sessao> getSessoesPorNomeFilme(String filme) {
+        return null;
+    }
+    public Double getMediaIngressosVendidos() {
+        return null;
+    }
+    public Double calcularTotalVendas() {
+        return null;
+    }
+}
