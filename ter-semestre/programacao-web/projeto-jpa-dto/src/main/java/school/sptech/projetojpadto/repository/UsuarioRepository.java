e0c62aae6888db2bfa998e356425f57d7e660eea
Bruno Henrique <bruno.lima@sptech.school>
2024-04-23 16:56:52 -0300

    fix: pastas

--- ter-semestre/programacao-web/projeto-jpa-dto/src/main/java/school/sptech/projetojpadto/repository/UsuarioRepository.java
+++
@@ -1,7 +0,0 @@
-package school.sptech.projetojpadto.repository;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-import school.sptech.projetojpadto.entity.Usuario;
-
-public interface UsuarioRepository extends JpaRepository<Usuario, Integer> {
-}
